4555506607c49b5ae42675b73f006ee84ba45b5e
raniiboe <61318713+raniiboe@users.noreply.github.com>
2023-11-19 13:42:59 +0100

    tried installing python

---
+++ Seyd/Mountain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seyd
+{
+    internal class Mountain
+    {
+        private (string, decimal)[] Owners;
+        private decimal[] Eves;
+        private decimal[] EveLambs;
+        private decimal[] RamLambs;
+
+        public Mountain((string, decimal)[] owners, decimal[] eves, decimal[] eveLambs, decimal[] ramLambs)
+        {
+            Owners = owners;
+            Eves = eves;
+            EveLambs = eveLambs;
+            RamLambs = ramLambs;
+        }
+    }
+}
